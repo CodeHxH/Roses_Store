e3474f905a5fc1312948f935e43ed0c12952c3f5
Agustin <rodriguezjoseagustin31@gmail.com>
2021-04-06 20:00:56 -0400

    listener de imagenes

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -43,6 +43,29 @@ document
 		});
 	});
 
+document
+	.querySelectorAll('.catalogo .product__card img')
+	.forEach((elemento) => {
+		elemento.addEventListener('click', () => {
+			const route = elemento.getAttribute('src');
+			const description = elemento.parentNode.parentNode.dataset.description;
+			const price = elemento.parentNode.parentNode.dataset.price;
+			const name = elemento.parentNode.parentNode.dataset.name;
+			const id = elemento.parentNode.parentNode.dataset.productid;
+
+			overlay.classList.add('active');
+			document.querySelector('#overlay img').src = route;
+			document.querySelector(
+				'#overlay .product__description'
+			).innerHTML = description;
+			document.querySelector('#overlay .product__price').innerHTML =
+				price + ' $';
+			document.querySelector('#overlay .product__name').innerHTML =
+				'COD. ' + name;
+			document.querySelector('#overlay .product__id').value = id;
+		});
+	});
+
 // Listener del boton para cerrar.
 document.querySelector('#btn-close').addEventListener('click', () => {
 	overlay.classList.remove('active');
